621b2262218b8a32c1bab2c8f6eb18e16eac52b3
Erwin van der Koogh <890386+evanderkoogh@users.noreply.github.com>
2021-12-12 16:03:20 +1100

    Adding support for importing html

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Env } from "./types"
+import { Env } from './types'
+import html from './index.html'
 
 export default {
   async fetch(request: Request, env: Env) {
-    return new Response('Hello World!')
-  }
+    return new Response(html, { headers: { 'content-type': 'text/html' } })
+  },
 }
